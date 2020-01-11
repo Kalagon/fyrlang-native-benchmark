1c71dd7294ac74db4fc4d5d45fdad53854c605cc
Oskar Carl <oskar.carl@uni-due.de>
2020-01-11 22:44:08 +0100

    Add tp_small

---
+++ simulated/tp_small/tp_small.c
@@ -0,0 +1,70 @@
+#include "tp_small.h"
+#include "measurement.h"
+
+void __init___285fca2a6b0bb45d151588769d4dc522882b1830e096e1b174b04e3914106b29() {
+
+};
+
+void Main_71128e6f4d7596de4f4b65bb1708c701ea80a086d698ced6798306f529b80275() {
+    start();
+    int v_i;
+    v_i = 0;
+    uintptr_t v_g_0;
+    v_g_0 = 0;
+    uintptr_t v_g_2;
+    v_g_2 = 0;
+    uintptr_t v_g_4;
+    v_g_4 = 0;
+    uintptr_t v_g_6;
+    v_g_6 = 0;
+    uintptr_t v_g_12;
+    v_g_12 = 0;
+    uintptr_t v_g_8;
+    v_g_8 = 0;
+    uintptr_t v_g_10;
+    v_g_10 = 0;
+    for (;;) {
+        bool tmp_1 = v_i < EVAL_LOOP_RUNS;
+        bool tmp_2 = !tmp_1;
+        if (tmp_2) {
+            break;
+        };
+        t_7c8c88665f5d7d690e67ae72d2de5b3729538848f0d1729c0f9e9f844488bf08 tmp_3 = (t_7c8c88665f5d7d690e67ae72d2de5b3729538848f0d1729c0f9e9f844488bf08){(int*)Malloc_5cdcff11cc4fe0b7702dcefd8e40a3205be2181cfbca6974337e1a1f974cd4e8(8, sizeof(int), &v_g_0), 1, 1};
+        t_7c8c88665f5d7d690e67ae72d2de5b3729538848f0d1729c0f9e9f844488bf08 v_arr0;
+        v_arr0 = tmp_3;
+        t_7c8c88665f5d7d690e67ae72d2de5b3729538848f0d1729c0f9e9f844488bf08 tmp_5 = (t_7c8c88665f5d7d690e67ae72d2de5b3729538848f0d1729c0f9e9f844488bf08){(int*)Malloc_5cdcff11cc4fe0b7702dcefd8e40a3205be2181cfbca6974337e1a1f974cd4e8(12, sizeof(int), &v_g_2), 1, 1};
+        t_7c8c88665f5d7d690e67ae72d2de5b3729538848f0d1729c0f9e9f844488bf08 v_arr1;
+        v_arr1 = tmp_5;
+        t_7c8c88665f5d7d690e67ae72d2de5b3729538848f0d1729c0f9e9f844488bf08 tmp_7 = (t_7c8c88665f5d7d690e67ae72d2de5b3729538848f0d1729c0f9e9f844488bf08){(int*)Malloc_5cdcff11cc4fe0b7702dcefd8e40a3205be2181cfbca6974337e1a1f974cd4e8(16, sizeof(int), &v_g_4), 1, 1};
+        t_7c8c88665f5d7d690e67ae72d2de5b3729538848f0d1729c0f9e9f844488bf08 v_arr2;
+        v_arr2 = tmp_7;
+        t_7c8c88665f5d7d690e67ae72d2de5b3729538848f0d1729c0f9e9f844488bf08 tmp_9 = (t_7c8c88665f5d7d690e67ae72d2de5b3729538848f0d1729c0f9e9f844488bf08){(int*)Malloc_5cdcff11cc4fe0b7702dcefd8e40a3205be2181cfbca6974337e1a1f974cd4e8(20, sizeof(int), &v_g_6), 1, 1};
+        t_7c8c88665f5d7d690e67ae72d2de5b3729538848f0d1729c0f9e9f844488bf08 v_arr3;
+        v_arr3 = tmp_9;
+        t_7c8c88665f5d7d690e67ae72d2de5b3729538848f0d1729c0f9e9f844488bf08 tmp_11 = (t_7c8c88665f5d7d690e67ae72d2de5b3729538848f0d1729c0f9e9f844488bf08){(int*)Malloc_5cdcff11cc4fe0b7702dcefd8e40a3205be2181cfbca6974337e1a1f974cd4e8(24, sizeof(int), &v_g_8), 1, 1};
+        t_7c8c88665f5d7d690e67ae72d2de5b3729538848f0d1729c0f9e9f844488bf08 v_arr4;
+        v_arr4 = tmp_11;
+        t_7c8c88665f5d7d690e67ae72d2de5b3729538848f0d1729c0f9e9f844488bf08 tmp_13 = (t_7c8c88665f5d7d690e67ae72d2de5b3729538848f0d1729c0f9e9f844488bf08){(int*)Malloc_5cdcff11cc4fe0b7702dcefd8e40a3205be2181cfbca6974337e1a1f974cd4e8(28, sizeof(int), &v_g_10), 1, 1};
+        t_7c8c88665f5d7d690e67ae72d2de5b3729538848f0d1729c0f9e9f844488bf08 v_arr5;
+        v_arr5 = tmp_13;
+        t_7c8c88665f5d7d690e67ae72d2de5b3729538848f0d1729c0f9e9f844488bf08 tmp_15 = (t_7c8c88665f5d7d690e67ae72d2de5b3729538848f0d1729c0f9e9f844488bf08){(int*)Malloc_5cdcff11cc4fe0b7702dcefd8e40a3205be2181cfbca6974337e1a1f974cd4e8(32, sizeof(int), &v_g_12), 1, 1};
+        t_7c8c88665f5d7d690e67ae72d2de5b3729538848f0d1729c0f9e9f844488bf08 v_arr6;
+        v_arr6 = tmp_15;
+        v_i++;
+        Free_17cd220e161d0642d8eb85204f15c78e318810795cb3cfa0160f7806603a731a(&v_g_0);
+        Free_17cd220e161d0642d8eb85204f15c78e318810795cb3cfa0160f7806603a731a(&v_g_2);
+        Free_17cd220e161d0642d8eb85204f15c78e318810795cb3cfa0160f7806603a731a(&v_g_4);
+        Free_17cd220e161d0642d8eb85204f15c78e318810795cb3cfa0160f7806603a731a(&v_g_6);
+        Free_17cd220e161d0642d8eb85204f15c78e318810795cb3cfa0160f7806603a731a(&v_g_12);
+        Free_17cd220e161d0642d8eb85204f15c78e318810795cb3cfa0160f7806603a731a(&v_g_8);
+        Free_17cd220e161d0642d8eb85204f15c78e318810795cb3cfa0160f7806603a731a(&v_g_10);
+    };
+    end();
+};
+
+int main(int argc, char **argv) {
+    __init___a0deb5ebe09db172d62667761dc6bc16118f6fb5e048a991b1ca841a830f76f0();
+    __init___285fca2a6b0bb45d151588769d4dc522882b1830e096e1b174b04e3914106b29();
+    Main_71128e6f4d7596de4f4b65bb1708c701ea80a086d698ced6798306f529b80275();
+    return 0;
+}
