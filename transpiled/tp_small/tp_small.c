53793468affd4903a73ef04ddaf5d98b7bc317ca
Oskar Carl <oskar.carl@uni-due.de>
2020-01-12 17:21:01 +0100

    Fix the transpiled sources for tp and tp_small

--- transpiled/tp_small/tp_small.c
+++ transpiled/tp_small/tp_small.c
@@ -8,20 +8,20 @@ void __init___285fca2a6b0bb45d151588769d4dc522882b1830e096e1b174b04e3914106b29()
 void Main_71128e6f4d7596de4f4b65bb1708c701ea80a086d698ced6798306f529b80275() {
     int v_i;
     v_i = 0;
+    uintptr_t v_g_12;
+    v_g_12 = 0;
     uintptr_t v_g_0;
     v_g_0 = 0;
-    uintptr_t v_g_2;
-    v_g_2 = 0;
     uintptr_t v_g_4;
     v_g_4 = 0;
     uintptr_t v_g_6;
     v_g_6 = 0;
-    uintptr_t v_g_12;
-    v_g_12 = 0;
-    uintptr_t v_g_8;
-    v_g_8 = 0;
+    uintptr_t v_g_2;
+    v_g_2 = 0;
     uintptr_t v_g_10;
     v_g_10 = 0;
+    uintptr_t v_g_8;
+    v_g_8 = 0;
     for (;;) {
         bool tmp_1 = v_i < EVAL_LOOP_RUNS;
         bool tmp_2 = !tmp_1;
@@ -57,13 +57,13 @@ void Main_71128e6f4d7596de4f4b65bb1708c701ea80a086d698ced6798306f529b80275() {
         t_7c8c88665f5d7d690e67ae72d2de5b3729538848f0d1729c0f9e9f844488bf08 v_arr6;
         v_arr6 = tmp_15;
         v_i++;
+        Free_17cd220e161d0642d8eb85204f15c78e318810795cb3cfa0160f7806603a731a(&v_g_12);
         Free_17cd220e161d0642d8eb85204f15c78e318810795cb3cfa0160f7806603a731a(&v_g_0);
-        Free_17cd220e161d0642d8eb85204f15c78e318810795cb3cfa0160f7806603a731a(&v_g_2);
         Free_17cd220e161d0642d8eb85204f15c78e318810795cb3cfa0160f7806603a731a(&v_g_4);
         Free_17cd220e161d0642d8eb85204f15c78e318810795cb3cfa0160f7806603a731a(&v_g_6);
-        Free_17cd220e161d0642d8eb85204f15c78e318810795cb3cfa0160f7806603a731a(&v_g_12);
-        Free_17cd220e161d0642d8eb85204f15c78e318810795cb3cfa0160f7806603a731a(&v_g_8);
+        Free_17cd220e161d0642d8eb85204f15c78e318810795cb3cfa0160f7806603a731a(&v_g_2);
         Free_17cd220e161d0642d8eb85204f15c78e318810795cb3cfa0160f7806603a731a(&v_g_10);
+        Free_17cd220e161d0642d8eb85204f15c78e318810795cb3cfa0160f7806603a731a(&v_g_8);
     };
 
 };
