314b5cdf54bbe6c7ee88b06f28c1f7655e322051
Oskar Carl <oskar.carl@uni-due.de>
2020-01-18 18:07:59 +0100

    Add prepare script for 32 bit systems

---
+++ prepare_32bit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source .env
+
+echo "Preparing the runtime sources for use on a 32 bit system..."
+
+sed -i 's/uint64_t/uint32_t/g' runtime/*/runtime.h runtime/*/runtime.c
+
+
+echo "Preparing the library headers..."
+sed -i 's/uint64_t/uint32_t/g' $FYRLIB_NATIVE/include/*/malloc.h
+
+
+echo "Cleaning previously built archives..."
+make clean
+
+echo "Done."
